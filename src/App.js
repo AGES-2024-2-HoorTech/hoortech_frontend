9a5c96823a2d434cb65d13b7d99d174795e916d4
Lukas <lukas.r@edu.pucrs.br>
2024-08-23 00:53:32 -0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hoor Tech em construção! Aguarde...</h1>
+        <p>
+          Esta poha vai sair nem q seja no ódio!!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
