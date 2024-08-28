958620bc96f7edbd08cbf04c2f8e59a8f7352471
Lukas <lukas.r@edu.pucrs.br>
2024-08-27 22:52:50 -0300

    Organizando a arquitetura do código

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles/index.css'; // Atualizado
+import App from './components/App/App'; // Atualizado
+import reportWebVitals from './utils/reportWebVitals'; // Atualizado
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +11,5 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Se você estiver utilizando o reportWebVitals, certifique-se de que ele está corretamente configurado.
 reportWebVitals();
