a2b5624683b63c8435a6cd4bf8c6a97cdf05478c
Lukas <lukas.r@edu.pucrs.br>
2024-09-08 16:42:08 -0300

    Revertendo o commit feito na develop por engano

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import Header from './components/header/headerComponent';
 
 function VideoCapture() {
-
   const videoRef = useRef(null);
   const [socket, setSocket] = useState(null);
   const [isSocketOpen, setIsSocketOpen] = useState(false);  // Verificação de estado do WebSocket
@@ -65,8 +63,6 @@ function VideoCapture() {
       });
 
     return () => {
-      <Header/>
-
       ws.close();
     };
   }, [isSocketOpen]);
@@ -81,7 +77,6 @@ function VideoCapture() {
   };
 
   return (
-    
     <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'flex-start', justifyContent: 'flex-start', height: '100vh', padding: '20px' }}>
       <div style={{
         position: 'relative',
@@ -93,7 +88,6 @@ function VideoCapture() {
         backgroundColor: '#f8f8f8',  // Coloca uma cor de fundo caso o vídeo não carregue
         marginRight: '20px'  // Adicione uma margem à direita para espaçamento
       }}>
-
         <video ref={videoRef} style={{ width: '100%', height: '100%', objectFit: 'cover' }} autoPlay></video>
         
         {/* Legenda sobre o vídeo */}
--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ import ReactDOM from 'react-dom/client'; // Correção da importação
 import './styles/index.css'; // Certo
 import App from './App.js'; // Atualizado
 import reportWebVitals from './utils/reportWebVitals'; // Atualizado
-import Header from './components/header/headerComponent.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root')); // Método correto para criar o root
 root.render(
-  
   <React.StrictMode>
-
-<Header/>
-<App/>
-</React.StrictMode>
+    <App /> {/* Renderizando o App que irá controlar toda a aplicação */}
+  </React.StrictMode>
 );
 
 // Se você estiver utilizando o reportWebVitals, certifique-se de que ele está corretamente configurado.
